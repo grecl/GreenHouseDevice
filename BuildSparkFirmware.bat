22b78daf08af744c3d42cc95df23470b8e8d0acf
Claudio Greuter <claudio.greuter@sonova.com>
2016-11-23 16:02:47 +0100

    Updated batch file to copy over custom "firmware" code

--- BuildSparkFirmware.bat
+++ BuildSparkFirmware.bat
@@ -1,5 +1,10 @@
-cd SparkOfficialFirmware\modules
+echo Copying all custom code over to official firmware
 
+xcopy /s SparkGreenhouseFirmware SparkOfficialFirmware
+
+echo Building and Flashing Firmware: Make sure to put device in DFU Mode
+
+cd SparkOfficialFirmware\modules
 make clean all PLATFORM=photon -s program-dfu
 
 pause
